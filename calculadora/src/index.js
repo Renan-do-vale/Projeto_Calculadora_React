ccf9e13cd398983eb481d28f98569603090eb979
Renan do vale ferreira <Renandovaleferreira@gmail.com>
2023-08-01 20:29:17 -0300

    up

---
+++ calculadora/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Calculator from './main/Calculator';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <h1>Calculator</h1>
+    <Calculator />
+  </div>
+
+);
+
+reportWebVitals();
