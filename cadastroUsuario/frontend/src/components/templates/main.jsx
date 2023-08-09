9d581fd43df0390fe1ef45baa64f18a53d421a32
Renan do vale ferreira <Renandovaleferreira@gmail.com>
2023-08-08 21:46:17 -0300

    up

---
+++ cadastroUsuario/frontend/src/components/templates/main.jsx
@@ -0,0 +1,11 @@
+import './main.css'
+import React from 'react'
+import Header from './header'
+
+export default props => 
+    <React.Fragment>
+        <Header />
+        <main className="content">
+            conteúdo
+        </main>
+    </React.Fragment>
---
+++ cadastroUsuario/frontend/src/main/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+
+import Logo from '../components/templates/logo'
+import Nav from '../components/templates/nav'
+import Main from '../components/templates/main'
+import Footer from '../components/templates/footer'
+
+export default props =>
+    <div className="app">
+        <Logo/>
+        <Nav/>
+        <Main/>
+        <Footer/>
+    </div>
